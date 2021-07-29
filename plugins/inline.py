f4d1e4638443b3b6dae3232e94c86cdf019faab1
james131989 <78683666+james131989@users.noreply.github.com>
2021-07-30 04:12:37 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -84,7 +84,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('Channel', url='https://t.me/taylorswift13fanpage')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
